f2c3737c39dbe0ed29d7737c991bb9c81bdc7989
Code Sengel <code.sengel@gmail.com>
2024-03-25 09:54:25 +0100

    Gateway Final + platformlistpublic

--- routes/gatewayRoutes.js
+++ routes/gatewayRoutes.js
@@ -1,48 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
-const others = require('axios');
-
-
-router.get('/test',(req,res) => {
-    res.json([{
-        'path':'HOME'
-    },{'chek':router}])
-})
+const axios = require("axios");
 
 router.get("/platformlistpublic", async (req, res) => {
-    console.log("la requete platformlistpubilc est arrivé coté gateway");
-    try {
-      console.log(
-        "Début du traitement de la requête platformlistpubilc côté gateway"
-      );
-  
-    
-        url = `${process.env.PUBLICDATA_URL}/platformlistpublic`;
-        console.log('voici url : ',url)
-        console.log('voici axios.default : ',axios.default)
-        console.log('voici axios.all : ',axios.all)
-  
-      
-     const response = await axios.default.get(url, {});
-     res.status(response.status).send(response.data);
-     
-
-
-    } catch (error) {
-      console.error(
-        "Erreur lors du traitement de la requête platformlistpubilc côté gateway :",
-        error
-      );
-      res
-        .status(500)
-        .send({"errorm":"Erreur lors de la récupération de la liste des plateformes publiques","error":error
-
-        }
-          
-        );
-    }
-  });
-
+  console.log("request arrived on gateway");
+  try {
+    console.log("before call");
+    url = `${process.env.PUBLICDATA_URL}/platformlistpublic`;
+    console.log("voici axios.default : ", axios.default);
+    const response = await axios.default.get(url, {});
+    console.log("after call");
+    res.status(response.status).send(response.data);
+  } catch (error) {
+    console.error(
+      "Error detected on gateway whith 'platformlistpublic' route ",
+      error
+    );
+    res.status(500).send({
+      errorm:
+        "Erreur lors de la récupération de la liste des plateformes publiques",
+      error: error,
+    });
+  }
+});
 
 module.exports = router;
