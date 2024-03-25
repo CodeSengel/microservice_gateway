c92275f66a11d977c7d14fc8c26c9df596855851
Code Sengel <code.sengel@gmail.com>
2024-03-25 11:15:05 +0100

    Gateway + authservice interface

--- functions-build/api.js
+++ functions-build/api.js
@@ -1,29 +1,21 @@
-const express = require('express')
-const serverless = require('serverless-http')
+const express = require("express");
+const serverless = require("serverless-http");
 const cors = require("cors");
 
 const dotenv = require("dotenv"); // Importez dotenv
 dotenv.config(); // Chargez les variables d'environnement à partir du fichier .env
-const gatewayRoutes = require('../routes/gatewayRoutes')
+const gatewayRoutes = require("../routes/gatewayRoutes");
 
-
-const app = express()
-const router = express.Router()
+const app = express();
 
 app.use(
-    cors({
-      origin: process.env.FRONTEND_URL,
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      allowedHeaders: ["Content-Type"],
-      credentials: true,
-    })
-  );
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  })
+);
 
-router.get('/',(req,res) => {
-    res.json([{
-        'path':'HOME'
-    },{'chek':router}])
-})
-
-app.use('/.netlify/functions/api/',gatewayRoutes)
-module.exports.handler = serverless(app)
+app.use("/.netlify/functions/api/", gatewayRoutes);
+module.exports.handler = serverless(app);
--- routes/gatewayRoutes.js
+++ routes/gatewayRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
+// public data service
 router.get("/platformlistpublic", async (req, res) => {
-  console.log("request arrived on gateway");
+  console.log("platformlistpublic request arrived on gateway");
   try {
     console.log("before call");
     url = `${process.env.PUBLICDATA_URL}/platformlistpublic`;
-    console.log("voici axios.default : ", axios.default);
     const response = await axios.default.get(url, {});
     console.log("after call");
     res.status(response.status).send(response.data);
@@ -17,8 +17,77 @@ router.get("/platformlistpublic", async (req, res) => {
       error
     );
     res.status(500).send({
-      errorm:
-        "Erreur lors de la récupération de la liste des plateformes publiques",
+      errorm: "Error detected on gateway whith 'platformlistpublic' route",
+      error: error,
+    });
+  }
+});
+
+// auth service
+
+router.get("/register", async (req, res) => {
+  console.log("register request arrived on gateway");
+  try {
+    console.log("before call", url);
+    url = `${process.env.AUTHSERVICE_URL}/register`;
+    const response = await axios.default.get(url, req.body);
+    console.log("after call");
+    res.status(response.status).send(response.data);
+  } catch (error) {
+    console.error("Error detected on gateway whith 'register' route ", error);
+    res.status(500).send({
+      errorm: "Error detected on gateway whith 'register' route",
+      error: error,
+    });
+  }
+});
+
+router.get("/login", async (req, res) => {
+  console.log("login request arrived on gateway");
+  try {
+    console.log("before call", url);
+    url = `${process.env.AUTHSERVICE_URL}/login`;
+    const response = await axios.default.get(url, req.body);
+    console.log("after call");
+    res.status(response.status).send(response.data);
+  } catch (error) {
+    console.error("Error detected on gateway whith 'login' route ", error);
+    res.status(500).send({
+      errorm: "Error detected on gateway whith 'login' route",
+      error: error,
+    });
+  }
+});
+
+router.get("/isloggedin", async (req, res) => {
+  console.log("isloggedin request arrived on gateway");
+  try {
+    console.log("before call", url);
+    url = `${process.env.AUTHSERVICE_URL}/isloggedin`;
+    const response = await axios.default.get(url);
+    console.log("after call");
+    res.status(response.status).send(response.data);
+  } catch (error) {
+    console.error("Error detected on gateway whith 'isloggedin' route ", error);
+    res.status(500).send({
+      errorm: "Error detected on gateway whith 'isloggedin' route",
+      error: error,
+    });
+  }
+});
+
+router.get("/logout", async (req, res) => {
+  console.log("logout request arrived on gateway");
+  try {
+    console.log("before call", url);
+    url = `${process.env.AUTHSERVICE_URL}/logout`;
+    const response = await axios.default.get(url);
+    console.log("after call");
+    res.status(response.status).send(response.data);
+  } catch (error) {
+    console.error("Error detected on gateway whith 'logout' route ", error);
+    res.status(500).send({
+      errorm: "Error detected on gateway whith 'logout' route",
       error: error,
     });
   }
